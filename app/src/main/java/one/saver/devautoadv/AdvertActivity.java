c7a7c2126ef8725fb7bf5b1741536ca58d5c34ca
gregorir <gregorir@saver.one>
2017-09-20 14:56:28 +0300

    Version 4 (Model and Color textView in AdvertActivity added)

--- app/src/main/java/one/saver/devautoadv/AdvertActivity.java
+++ app/src/main/java/one/saver/devautoadv/AdvertActivity.java
@@ -3,25 +3,28 @@ package one.saver.devautoadv;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AdvertActivity extends Activity {
     TextView textMake;
     Spinner spinnerPrice;
     Spinner spinnerMinPrice;
     Spinner spinnerMaxPrice;
-    String[] prise = {"No minimum", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
-    String[] priseMin = {"No minimum", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
-    String[] priseMax = {"No maximum", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
+    String[] price = {"No minimum", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
+    String[] priceMin;
+    String[] priceMax;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advert);
         textMake = (TextView) findViewById(R.id.textMake);
-        spinnerPrice = (Spinner) findViewById(R.id.spinnerPrice);
+     //   spinnerPrice = (Spinner) findViewById(R.id.spinnerPrice);
         spinnerMinPrice = (Spinner) findViewById(R.id.spinnerMinPrice);
         spinnerMaxPrice = (Spinner) findViewById(R.id.spinnerMaxPrice);
         // get intent data
@@ -32,17 +35,44 @@ public class AdvertActivity extends Activity {
         ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
         imageView.setImageResource(imageAdapter.mThumbIds[position]);
         textMake.setText(Integer.toString(position));
-        ArrayAdapter<String> adapterPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, prise);
+  /*      ArrayAdapter<String> adapterPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, price);
         // Определяем разметку для использования при выборе элемента
         adapterPrice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Применяем адаптер к элементу spinner
-        spinnerPrice.setAdapter(adapterPrice);
-        ArrayAdapter<String> adapterMinPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, priseMin);
+        spinnerPrice.setAdapter(adapterPrice);   */
+        priceMin = getResources().getStringArray(R.array.priseMin);
+        ArrayAdapter<String> adapterMinPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                priceMin);
         adapterMinPrice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMinPrice.setAdapter(adapterMinPrice);
-        ArrayAdapter<String> adapterMaxPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, priseMax);
+        priceMax = getResources().getStringArray(R.array.priseMax);
+        ArrayAdapter<String> adapterMaxPrice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                priceMax);
         adapterMaxPrice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMaxPrice.setAdapter(adapterMaxPrice);
-
+        AdapterView.OnItemSelectedListener spinnerMinPriceListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplicationContext(), spinnerMinPrice.getSelectedItem().toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        };
+        spinnerMinPrice.setOnItemSelectedListener(spinnerMinPriceListener);
+        AdapterView.OnItemSelectedListener spinnerMaxPriceListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplicationContext(), spinnerMaxPrice.getSelectedItem().toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        };
+        spinnerMaxPrice.setOnItemSelectedListener(spinnerMaxPriceListener);
     }
 }
